d86b9ad4e86bccba4d03d407eb5b379f468c6788
rgederin <ruslan.gederin@goeuro.com>
2017-11-13 14:37:59 +0200

    display recipe detailed view

---
+++ src/test/java/com/gederin/controller/RecipeControllerTest.java
@@ -0,0 +1,53 @@
+package com.gederin.controller;
+
+
+import com.gederin.model.Recipe;
+import com.gederin.service.RecipeService;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RecipeControllerTest {
+
+    @Mock
+    private RecipeService recipeService;
+
+    private RecipeController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new RecipeController(recipeService);
+    }
+
+    @Test
+    public void shouldGetRecipe() throws Exception {
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        when(recipeService.findRecipeById(anyLong())).thenReturn(recipe);
+
+        mockMvc.perform(get("/recipe/show/1"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/show"))
+                .andExpect(model().attributeExists("recipe"));
+    }
+}
