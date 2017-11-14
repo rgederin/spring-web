1b7276cd0f0c9d27c44fcd31b51f9336b1b03b64
rgederin <ruslan.gederin@goeuro.com>
2017-11-14 15:45:10 +0200

    add new recipe

--- src/main/java/com/gederin/controller/RecipeController.java
+++ src/main/java/com/gederin/controller/RecipeController.java
@@ -1,11 +1,14 @@
 package com.gederin.controller;
 
+import com.gederin.command.RecipeCommand;
 import com.gederin.service.RecipeService;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import lombok.AllArgsConstructor;
 
@@ -14,11 +17,26 @@ import lombok.AllArgsConstructor;
 public class RecipeController {
     private RecipeService recipeService;
 
-    @RequestMapping("/recipe/show/{id}")
+    @GetMapping(path = "/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model) {
 
         model.addAttribute("recipe", recipeService.findRecipeById(Long.valueOf(id)));
 
         return "recipe/show";
+    }
+
+    @GetMapping(path = "/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+
+    @PostMapping(path = "/recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
