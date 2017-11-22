806843a9cb3a3f160a70eb0760a281a5738718a0
rgederin <ruslan.gederin@goeuro.com>
2017-11-22 19:01:28 +0200

    save image in db

--- src/main/java/com/gederin/service/ImageServiceImpl.java
+++ src/main/java/com/gederin/service/ImageServiceImpl.java
@@ -1,17 +1,45 @@
 package com.gederin.service;
 
+import com.gederin.model.Recipe;
+import com.gederin.repository.RecipeRepository;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
+@AllArgsConstructor
 public class ImageServiceImpl implements ImageService {
+
+    private final RecipeRepository recipeRepository;
+
     @Override
     public void saveImageFile(Long recipeId, MultipartFile file) {
 
-        log.debug("Received a file");
+        try {
+            Recipe recipe = recipeRepository.findById(recipeId).get();
 
+            Byte[] byteObjects = new Byte[file.getBytes().length];
+
+            int i = 0;
+
+            for (byte b : file.getBytes()) {
+                byteObjects[i++] = b;
+            }
+
+            recipe.setImage(byteObjects);
+
+            recipeRepository.save(recipe);
+        } catch (IOException e) {
+            //todo handle better
+            log.error("Error occurred", e);
+
+            e.printStackTrace();
+        }
     }
 }
