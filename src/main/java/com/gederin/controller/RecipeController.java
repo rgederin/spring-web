1dbc8f4cec686640fb90554f74b06d8f92f246c9
rgederin <ruslan.gederin@goeuro.com>
2017-11-23 11:38:42 +0200

    add number format exception handling

--- src/main/java/com/gederin/controller/RecipeController.java
+++ src/main/java/com/gederin/controller/RecipeController.java
@@ -63,7 +63,7 @@ public class RecipeController {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception){
+    public ModelAndView handleNotFound(Exception exception) {
 
         log.error("Handling not found exception");
         log.error(exception.getMessage());
@@ -73,6 +73,21 @@ public class RecipeController {
         modelAndView.setViewName("404error");
         modelAndView.addObject("exception", exception);
 
+
+        return modelAndView;
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception) {
+
+        log.error("Handling Number Format Exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("400error");
+        modelAndView.addObject("exception", exception);
 
         return modelAndView;
     }
