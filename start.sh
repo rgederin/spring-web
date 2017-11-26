036ca52a7c5de17ec811df5ca4cd34b5359483f8
rgederin <ruslan.gederin@goeuro.com>
2017-11-27 00:26:04 +0200

    docerized mysql

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql || docker start mysqldb
 
-docker run --name recipe-app -p 8080:8080 -d spring-web-image
+docker run --name recipe-app -p 8080:8080 -e SPRING_PROFILES_ACTIVE=dev --link mysqldb:mysql -d spring-web-image
